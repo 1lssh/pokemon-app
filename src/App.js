d28da43f79d658b141b066654e8571e65906de7d
1lssh <80113936+1lssh@users.noreply.github.com>
2023-08-23 19:50:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { Chip } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [pokemons, setPokemons] = useState()
+	const [toggleTab, setToggleTab] = useState()
+
+	const getPokemons = async () => {
+		const pokemons = []
+		for (let i = 1; i < 11; i++) {
+			const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+			pokemons.push(response.data)
+		}
+		setPokemons(pokemons)
+		setToggleTab(pokemons[0])
+	}
+
+	useEffect(() => {
+		getPokemons()
+	}, [])
+
+
+	return (
+		<div className='container'>
+			<div className='pokemon'>
+				<div className='pokemon__options'>
+					{
+						pokemons &&
+						pokemons.map(pokemon => {
+							return (
+								<div
+									className='pokemon__option'
+									key={pokemon.id}
+									onClick={() => setToggleTab(pokemon)}
+								>
+									<Chip color="info" label={pokemon.name} />
+								</div>
+							)
+						})
+					}
+				</div>
+				<div className='pokemon__info'>
+					{
+						toggleTab &&
+						<div>
+							<h1 className='pokemon__title'>{toggleTab.name}</h1>
+							<div className='pokemon__img'>
+								<img src={toggleTab.sprites.front_default} alt='' />
+							</div>
+							<div className='pokemon__description'>id: {toggleTab.id}</div>
+							<div className='pokemon__description'>heigth: {toggleTab.height}</div>
+							<div className='pokemon__description'>weight: {toggleTab.weight}</div>
+							<div className='pokemon__description'>attack: {toggleTab.stats[1].base_stat}</div>
+						</div>
+					}
+				</div>
+			</div >
+		</div>
+
+	);
 }
 
 export default App;
